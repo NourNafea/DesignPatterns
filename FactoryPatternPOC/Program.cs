1fbfc98befa18f9baec80bd78131a4aa69342696
NourNafea <nour68.nafea@gmail.com>
2022-12-10 22:15:56 +0200

    init

---
+++ FactoryPatternPOC/Program.cs
@@ -0,0 +1,11 @@
+﻿using FactoryPatternPOC;
+
+
+var cardDetails = CreditCardFactory.GetCreditCart( Console.ReadLine()!);
+
+if(cardDetails != null)
+{
+    Console.WriteLine(cardDetails.GetCardType());
+    Console.WriteLine(cardDetails.GetCreditLimit());
+    Console.WriteLine(cardDetails.GetAnnualCharge());
+}
---
+++ FactoryPatternPOC/Types/Titanium.cs
@@ -0,0 +1,20 @@
+﻿namespace FactoryPatternPOC.Types;
+
+public class Titanium : ICreditCard
+{
+    public string GetCardType()
+    {
+        return "Titanium Edge";
+    }
+    
+    public int GetCreditLimit()
+    {
+        return 25000;
+    }
+    
+    public int GetAnnualCharge()
+    {
+        return 1500;
+    }
+    
+}
