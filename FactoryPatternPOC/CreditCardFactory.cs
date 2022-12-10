7cc38a956118977e96e41b8355ed15bfa844fbee
NourNafea <nour68.nafea@gmail.com>
2022-12-10 22:18:24 +0200

    init

---
+++ FactoryPatternPOC/CreditCardFactory.cs
@@ -0,0 +1,17 @@
+﻿using FactoryPatternPOC.Types;
+
+namespace FactoryPatternPOC;
+
+public class CreditCardFactory
+{
+    public static ICreditCard? GetCreditCart(string cardType)
+    {
+        return cardType switch
+        {
+            "MoneyBack" => new MoneyBack(),
+            "Titanium" => new Titanium(),
+            "Platinum" => new Platinum(),
+            _ => null,
+        };
+    }
+}
